f83e160444ccd1d25da1e20beac9f98b1f432a89
Francisca <Franiicarrillo@gmail.com>
2018-05-31 16:02:53 -0400

    guardar valores, y unir estructura

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 /* Acá va tu código */
+obtener = () => {
+
+let texto = document.getElementById("Caja").value;
+let offset = parseInt(document.getElementById("Caja2").value);
+document.getElementById("Caja3").innerHTML=window.cipher.encode(offset,texto);
+
+}
+
+Transformar =() =>{
+  let texto = document.getElementById("Caja").value;
+  let offset = parseInt(document.getElementById("Caja2").value);
+document.getElementById("Caja3").innerHTML=window.cipher.decode(offset,texto);
+}
