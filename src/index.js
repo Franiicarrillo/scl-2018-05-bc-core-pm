0ecda157c81569bad7e09be8a8386b849b47681d
Francisca <Franiicarrillo@gmail.com>
2018-05-31 17:36:57 -0400

    Modifique estructura

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* Acá va tu código */
 obtener = () => {
 
-let texto = document.getElementById("Caja").value;
-let offset = parseInt(document.getElementById("Caja2").value);
-document.getElementById("Caja3").innerHTML=window.cipher.encode(offset,texto);
+let texto = document.getElementById("Caja2").value;
+let offset = parseInt(document.getElementById("Caja").value);
+document.getElementById("Caja3").innerHTML=window.cipher.encode(texto,offset);
 
 }
 
 Transformar =() =>{
-  let texto = document.getElementById("Caja").value;
-  let offset = parseInt(document.getElementById("Caja2").value);
-document.getElementById("Caja3").innerHTML=window.cipher.decode(offset,texto);
+  let texto = document.getElementById("Caja2").value;
+  let offset = parseInt(document.getElementById("Caja").value);
+document.getElementById("Caja3").innerHTML=window.cipher.decode(texto,offset);
 }
